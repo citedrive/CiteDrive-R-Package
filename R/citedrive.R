7d452b396fd5368862cb8dd6cc58664984fbc643
garethbilaney <gareth.bilaney@hhu.de>
2022-03-28 18:18:40 +0200

    ...

---
+++ R/citedrive.R
@@ -0,0 +1,9 @@
+
+getBib <- function(name, url) {
+    fileName <- paste(name,".bib",sep="")
+    z <- readLines(url)
+    fileConn<-file(fileName)
+    writeLines(z, fileConn)
+    close(fileConn)
+}
+
