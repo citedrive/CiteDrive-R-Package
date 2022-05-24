1be7eb18172eb825b9343eeef13877e458e07fb2
Teela <34556944+DrPen@users.noreply.github.com>
2022-05-25 00:01:15 +0200

    revised functionalities and code cleanup

--- R/semanticScholar.R
+++ R/semanticScholar.R
@@ -1,5 +1,10 @@
 # fetches meta data through DOI from semantic scholar using api calls
-# toDo: Replace dummy bibtex file with actual bibtex
+# toDo: Decide whether to use citationKey or DOI to fetch data
+# potential idea: use regex provided here:
+# http://web.archive.org/web/20180321212859/https://www.crossref.org/blog/dois-and-matching-regular-expressions/
+# to check if DOI or citationKey was used
+
+#toDo: remove dummy DOI by paper functions
 
 require(httr)
 require(jsonlite)
@@ -10,57 +15,124 @@ require(jsonlite)
 #}
 ###################
 
-citationCount <- function(citationKey){
+#data sourced by paper#
+abstract <- function(DOIorPaperID){
   #DOI<-getDOI(citationKey)
-  DOI <- "10.2337/dc12-1805"
-  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOI,"?fields=title,citationCount", sep="")
-  res <- fromJSON(rawToChar(GET(APICall)$content))
-  print(paste(res$title, " was cited ", res$citationCount, " times", sep = ""))
-}
-
-authors <- function(citationKey){
-  #DOI<-getDOI(citationKey)
-  DOI <- "10.7759/cureus.r53"
-  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOI,"?fields=title,authors", sep="")
-  res <- fromJSON(rawToChar(GET(APICall)$content))
-  print(res)
-  print(paste(res$title, " was written by:", sep = ""))
-  print(res$authors)
-}
-
-abstract <- function(citationKey){
-  #DOI<-getDOI(citationKey)
-  DOI <- "10.2337/dc12-1805"
-  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOI,"?fields=title,abstract", sep="")
+  DOIorPaperID <- "10.2337/dc12-1805"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=title,abstract", sep="")
   res <- fromJSON(rawToChar(GET(APICall)$content))
   print(paste("Abstract for ", res$title, ":", sep = ""))
-  print(res$abstract)
+  return(res$abstract)
+}
+
+#may be truncated
+authors <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.7759/cureus.r53"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=title,authors", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  print(res)
+  print(paste(res$title, "was written by:"))
+  return(res$authors)
+}
+
+citationCount <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.2337/dc12-1805"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=title,citationCount", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  print(paste("Citation count for ", res$title, ":", sep = ""))
+  return(res$citationCount)
+}
+
+fieldsOfStudy <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.2337/dc12-1805"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=title,fieldsOfStudy", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  print(paste("Fields of study for ", res$title, ":", sep = ""))
+  return(res$fieldsOfStudy)
+}
+
+influentialCitationCount <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.2337/dc12-1805"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=title,influentialCitationCount", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  print(paste("Influential citationCount count for ", res$title, ":", sep = ""))
+  return(res$influentialCitationCount)
+}
+
+isOpenAccess <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.2337/dc12-1805"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=isOpenAccess", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  return(res$isOpenAccess)
+}
+
+paperURL <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.1021/jacs.9b13484"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=title,url", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  browseURL(res$URL)
+  print(paste("Sematic Scholar URL for ", res$title, ":", sep = ""))
+  return(res$tldr)
+}
+
+references <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.1021/jacs.9b13484"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"/references", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  print(paste("References for paper ", res$title, ":", sep = ""))
+  return(res)
+}
+
+referenceCount <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.2337/dc12-1805"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=title,referenceCount", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  print(paste("Reference count for ", res$title, ":", sep = ""))
+  return(res$referenceCount)
+}
+
+tldr <- function(DOIorPaperID){
+  #DOI<-getDOI(citationKey)
+  DOIorPaperID <- "10.1021/jacs.9b13484"
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/paper/",DOIorPaperID,"?fields=title,tldr", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  print(paste("Short summary of ", res$title, ":", sep = ""))
+  return(res$tldr)
+}
+########################
+
+
+#data sourced by author#
+authorInfo <- function(authorID){
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/author/", authorID, "/?fields=name,hIndex,aliases", sep="")
+  res <- fromJSON(rawToChar(GET(APICall)$content))
+  APICall <- paste("https://api.semanticscholar.org/graph/v1/author/", authorID, "/papers", sep="")
+  res$paperCount <-  nrow(fromJSON(rawToChar(GET(APICall)$content))$data)
+  return(res)
 }
 
 findAuthor <- function(authorName){
   APICall <- paste("https://api.semanticscholar.org/graph/v1/author/search?query=", gsub(" ", "+", authorName), sep="")
   res <- fromJSON(rawToChar(GET(APICall)$content))
-  print(res$data)
+  return(res$data)
 }
 
 papersByAuthor <- function(authorID){
   #authorID <- getAuthorID(citationKey)
-  #APICall <- paste("https://api.semanticscholar.org/graph/v1/author/", authorID, "?fields=paperId,title", sep="")
   APICall <- paste("https://api.semanticscholar.org/graph/v1/author/", authorID, "/papers/?fields=year,title,venue,isOpenAccess,externalIds", sep="")
   res <- fromJSON(rawToChar(GET(APICall)$content))
   #toDo pick best method for print out
   print(res$data)
   print("\nor\n")
   subset(res$data$externalIds <- subset(res$data$externalIds, select=c(DOI)))
-  print(subset(res$data, select=c(externalIds, title, venue, year, isOpenAccess)))
+  return(subset(res$data, select=c(externalIds, title, venue, year, isOpenAccess)))
 }
-
-authorInfo <- function(authorID){
-  APICall <- paste("https://api.semanticscholar.org/graph/v1/author/", authorID, "/?fields=name,hIndex,aliases,papers", sep="")
-  res <- fromJSON(rawToChar(GET(APICall)$content))
-  print(res)
-}
-
-#https://api.semanticscholar.org/graph/v1/paper/10.2337/dc12-1805?fields=title,citations.authors
-#"https://api.semanticscholar.org/graph/v1/paper/10.1109/tcyb.2017.2751646?fields=citationCount"
-
+########################
