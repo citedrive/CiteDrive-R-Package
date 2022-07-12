1a2e972889f6e59aeae8636a6b933e2651e068bb
Teela <34556944+DrPen@users.noreply.github.com>
2022-07-12 19:21:13 +0200

    Removed R/citedrive.R and R/fetchcsl.R - no longer needed, R-MD processes links directly

--- R/citedrive.R
+++
@@ -1,8 +0,0 @@
-getBib <- function(name, url) {
-    fileName <- paste(name,".bib",sep="")
-    z <- readLines(url)
-    fileConn<-file(fileName)
-    writeLines(z, fileConn)
-    close(fileConn)
-}
-
