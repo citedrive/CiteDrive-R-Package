eea42a076ca9dacc7f9a05c53c0abb42ff7b749c
Teela <34556944+DrPen@users.noreply.github.com>
2022-08-01 02:20:22 +0200

    Added journal evaluation functions toDo: Add to results

--- R/crossref.R
+++ R/crossref.R
@@ -36,23 +36,23 @@ URLCrossRef <- function(DOI){
 # input:  String (author's name) and int (number of expected results)
 # output: R Object
 # Description: Fetches desired amount of works by a given author
-findWorksByAuthor <- function(authorName,pageSize=20){
+findWorksByAuthor <- function(authorName,numberOfResults=20){
   # assures that page size is between 1 and 1000 to allow maximum amount of data
   # to be feched in one call as per API definition
   # page sizes > 1000 are fetched and appended afterwards
-  if(!is.numeric(pageSize) || pageSize < 1){
-    pageSize <- 20
-    actualPageSize <- 20
+  if(!is.numeric(numberOfResults) || numberOfResults < 1){
+    numberOfResults <- 20
+    actualnumberOfResults <- 20
   }
-  else if(pageSize <= 1000) {
-    actualPageSize <- pageSize
+  else if(numberOfResults <= 1000) {
+    actualnumberOfResults <- numberOfResults
   }
   else {
-    actualPageSize <- pageSize
-    pageSize <- 1000
+    actualnumberOfResults <- numberOfResults
+    numberOfResults <- 1000
   }
 
-  APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author&query.author=", gsub(" ", "+", authorName),"&rows=",pageSize, sep="")
+  APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author&query.author=", gsub(" ", "+", authorName),"&rows=",numberOfResults, sep="")
   res <- GET(APICall, user_agent(userAgentHeader))
 
   if(res$status == "200" && fromJSON(rawToChar(res$content))$message$`total-results` > 0){
@@ -60,42 +60,42 @@ findWorksByAuthor <- function(authorName,pageSize=20){
     resTotal <- res$message$`total-results`
 
     # prevents fetching of data larger than the maximum entires available
-    if(resTotal < pageSize){
-      pageSize <- resTotal
-      APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author&query.author=", gsub(" ", "+", authorName),"&rows=",pageSize, sep="")
+    if(resTotal < numberOfResults){
+      numberOfResults <- resTotal
+      APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author&query.author=", gsub(" ", "+", authorName),"&rows=",numberOfResults, sep="")
       res <- GET(APICall, user_agent(userAgentHeader))
       res <- fromJSON(rawToChar(res$content))
       resTotal <- res$message$`total-results`
     }
 
     # fetch and append results > 1000
-    if(actualPageSize > 1000){
+    if(actualnumberOfResults > 1000){
       # prevents fetching of data larger than the maximum entires available
-      if(resTotal < actualPageSize){
-        actualPageSize <- resTotal
+      if(resTotal < actualnumberOfResults){
+        actualnumberOfResults <- resTotal
       }
-      tmpPageSize <- actualPageSize
-      tmpPageSize <- tmpPageSize - 1000
+      tmpnumberOfResults <- actualnumberOfResults
+      tmpnumberOfResults <- tmpnumberOfResults - 1000
       offset <- 1000
       resTemp <- res
 
 
-      while(tmpPageSize > 0){
-        #APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author,published&query.author=", gsub(" ", "+", authorName),"&rows=",tmpPageSize,"&offset=",offset , sep="")
-        APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author&query.author=", gsub(" ", "+", authorName),"&rows=",tmpPageSize,"&offset=",offset , sep="")
+      while(tmpnumberOfResults > 0){
+        #APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author,published&query.author=", gsub(" ", "+", authorName),"&rows=",tmpnumberOfResults,"&offset=",offset , sep="")
+        APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author&query.author=", gsub(" ", "+", authorName),"&rows=",tmpnumberOfResults,"&offset=",offset , sep="")
         res <- GET(APICall, user_agent(userAgentHeader))
         res <- fromJSON(rawToChar(res$content))
 
         resTemp$message$items <- rbind(resTemp$message$items, res$message$items)
 
-        tmpPageSize <- tmpPageSize - 1000
+        tmpnumberOfResults <- tmpnumberOfResults - 1000
         offset <- offset + 1000
       }
       res$message$items <- resTemp$message$items
-      pageSize <- actualPageSize
+      numberOfResults <- actualnumberOfResults
     }
-    print(paste("Returning", pageSize, "out of",resTotal, "results"))
-    print("To query more results provide findWorksByAuthor with the desired pageSize parameter")
+    print(paste("Returning", numberOfResults, "out of",resTotal, "results"))
+    print("To query more results provide findWorksByAuthor with the desired numberOfResults parameter")
 
     return(res$message$items)
   }
@@ -119,3 +119,200 @@ referencesCrossRef <- function(DOI){
   print(paste("DOI", DOI, "was found in Crossref's Database"))
   return()
 }
+
+# input:  String (DOI)
+# output: R Object or NULL
+# Description: Returns Name ISSN of publishing journal
+workPublishedBy <- function(DOI){
+  APICall <- paste("https://api.crossref.org/works/", DOI, sep="")
+  res <- GET(APICall, user_agent(userAgentHeader))
+  if(res$status == "200"){
+    res <- fromJSON(rawToChar(res$content))$message
+    paperTitle <- res$title
+    res <- data.frame(ISSN = res$ISSN)
+    res$Title <-JournalInfo(res$ISSN)$title
+    res$Publisher <- JournalInfo(res$ISSN)$publisher
+    print(paste("Printing Journal Infomation for", paperTitle, sep = " "))
+    return(res)
+  }
+  print(paste("DOI", DOI, "was found in Crossref's Database"))
+  return()
+}
+
+# input:  String (Journal Subjects) and int (number of expected results)
+# output: R Object
+# Description: Fetches desired amount of Journals by a given subject
+findJournalByKeywords <- function(Keywords, numberOfResults=20){
+  # assures that page size is between 1 and 1000 to allow maximum amount of data
+  # to be feched in one call as per API definition
+  # page sizes > 1000 are fetched and appended afterwards
+  if(!is.numeric(numberOfResults) || numberOfResults < 1){
+    numberOfResults <- 20
+    actualnumberOfResults <- 20
+  }
+  else if(numberOfResults <= 1000) {
+    actualnumberOfResults <- numberOfResults
+  }
+  else {
+    actualnumberOfResults <- numberOfResults
+    numberOfResults <- 1000
+  }
+
+  APICall <- paste("https://api.crossref.org/journals?query=", gsub(" ", "+", Keywords),"&rows=",numberOfResults, sep="")
+  res <- GET(APICall, user_agent(userAgentHeader))
+
+  if(res$status == "200" && fromJSON(rawToChar(res$content))$message$`total-results` > 0){
+    res <- fromJSON(rawToChar(res$content))
+    resTotal <- res$message$`total-results`
+
+    # prevents fetching of data larger than the maximum entires available
+    if(resTotal < numberOfResults){
+      numberOfResults <- resTotal
+      APICall <- paste("https://api.crossref.org/journals?query=", gsub(" ", "+", Keywords),"&rows=",numberOfResults, sep="")
+      res <- GET(APICall, user_agent(userAgentHeader))
+      res <- fromJSON(rawToChar(res$content))
+      resTotal <- res$message$`total-results`
+    }
+
+    # fetch and append results > 1000
+    if(actualnumberOfResults > 1000){
+      # prevents fetching of data larger than the maximum entires available
+      if(resTotal < actualnumberOfResults){
+        actualnumberOfResults <- resTotal
+      }
+      tmpnumberOfResults <- actualnumberOfResults
+      tmpnumberOfResults <- tmpnumberOfResults - 1000
+      offset <- 1000
+      resTemp <- res
+
+
+      while(tmpnumberOfResults > 0){
+        #APICall <- paste("https://api.crossref.org/works?select=DOI,type,title,is-referenced-by-count,references-count,author,published&query.author=", gsub(" ", "+", authorName),"&rows=",tmpnumberOfResults,"&offset=",offset , sep="")
+        APICall <- paste("https://api.crossref.org/journals?query=", gsub(" ", "+", Keywords), "&rows=",tmpnumberOfResults, "&offset=", offset, sep="")
+        res <- GET(APICall, user_agent(userAgentHeader))
+        res <- fromJSON(rawToChar(res$content))
+
+        resTemp$message$items <- rbind(resTemp$message$items, res$message$items)
+
+        tmpnumberOfResults <- tmpnumberOfResults - 1000
+        offset <- offset + 1000
+      }
+      res$message$items <- resTemp$message$items
+      numberOfResults <- actualnumberOfResults
+    }
+    print(paste("Returning", numberOfResults, "out of",resTotal, "results"))
+    print("To query more results provide findJournalByKeywords with the desired numberOfResults parameter")
+
+    return(res$message$items)
+  }
+  else{
+    print(paste("No Journals for", Keywords, "have been found in crossref's database"))
+    return()
+  }
+}
+
+# input:  String (Journal ISSN)
+# output: R Object
+# Description: Fetches inforamtion about Journal by ISSN
+JournalInfo <- function(ISSN){
+  APICall <- paste("https://api.crossref.org/journals/", ISSN, sep="")
+  res <- GET(APICall, user_agent(userAgentHeader))
+  if(res$status == "200"){
+    res <- fromJSON(rawToChar(res$content))$message
+    print(paste("Printing Journal information for",res$title))
+    return(res)
+  }
+  else{
+    print(paste("No journal with ISSN:",ISSN,"was found in crossref's database"))
+    return()
+  }
+}
+
+# input:  String (Journal ISSN)
+# output: R Object
+# Description: Fetches inforamtion about Journal by ISSN
+JournalInfo <- function(ISSN){
+  APICall <- paste("https://api.crossref.org/journals/", ISSN, sep="")
+  res <- GET(APICall, user_agent(userAgentHeader))
+  if(res$status == "200"){
+    res <- fromJSON(rawToChar(res$content))$message
+    print(paste("Printing Journal information for", res$title, "published in", res$publisher))
+    return(res)
+  }
+  else{
+    print(paste("No journal with ISSN:",ISSN,"was found in crossref's database"))
+    return()
+  }
+}
+
+# input:  String (Journal ISSN)
+# output: R Object
+# Description: Fetches inforamtion about Journal by ISSN
+JournalWorks <- function(ISSN, numberOfResults=20){
+  # assures that page size is between 1 and 1000 to allow maximum amount of data
+  # to be feched in one call as per API definition
+  # page sizes > 1000 are fetched and appended afterwards
+  if(!is.numeric(numberOfResults) || numberOfResults < 1){
+    numberOfResults <- 20
+    actualnumberOfResults <- 20
+  }
+  else if(numberOfResults <= 1000) {
+    actualnumberOfResults <- numberOfResults
+  }
+  else {
+    actualnumberOfResults <- numberOfResults
+    numberOfResults <- 1000
+  }
+
+  APICall <- paste("https://api.crossref.org/journals/", ISSN,"/works?", "rows=", numberOfResults, sep="")
+  res <- GET(APICall, user_agent(userAgentHeader))
+  print(APICall)
+
+  if(res$status == "200" && fromJSON(rawToChar(res$content))$message$`total-results` > 0){
+    res <- fromJSON(rawToChar(res$content))
+    resTotal <- res$message$`total-results`
+
+    # prevents fetching of data larger than the maximum entires available
+    if(resTotal < numberOfResults){
+      numberOfResults <- resTotal
+      APICall <- paste("https://api.crossref.org/journals/", ISSN,"/works", "?rows=",numberOfResults, sep="")
+      res <- GET(APICall, user_agent(userAgentHeader))
+      res <- fromJSON(rawToChar(res$content))
+      resTotal <- res$message$`total-results`
+    }
+
+    # fetch and append results > 1000
+    if(actualnumberOfResults > 1000){
+      # prevents fetching of data larger than the maximum entires available
+      if(resTotal < actualnumberOfResults){
+        actualnumberOfResults <- resTotal
+      }
+      tmpnumberOfResults <- actualnumberOfResults
+      tmpnumberOfResults <- tmpnumberOfResults - 1000
+      offset <- 1000
+      resTemp <- res
+
+
+      while(tmpnumberOfResults > 0){
+        APICall <- paste("https://api.crossref.org/journals/", ISSN,"/works", "?rows=",tmpnumberOfResults, "&offset=", offset, sep="")
+        res <- GET(APICall, user_agent(userAgentHeader))
+        res <- fromJSON(rawToChar(res$content))
+
+        resTemp$message$items <- rbind(resTemp$message$items, res$message$items)
+
+        tmpnumberOfResults <- tmpnumberOfResults - 1000
+        offset <- offset + 1000
+      }
+      res$message$items <- resTemp$message$items
+      numberOfResults <- actualnumberOfResults
+    }
+    print(paste("Returning", numberOfResults, "out of",resTotal, "results"))
+    print("To query more results provide JournalWorks with the desired numberOfResults parameter")
+    return(res$message$items)
+  }
+  else{
+    print(paste("No Journals for", ISSN, "have been found in crossref's database"))
+    return()
+  }
+}
+
