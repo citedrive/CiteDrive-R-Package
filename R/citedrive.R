549ff2b68c3d2ec2e3b6643bb7a3c06d8d9ae789
Teela <34556944+DrPen@users.noreply.github.com>
2022-09-09 00:25:29 +0200

    removed dummy debug variable

--- R/citedrive.R
+++ R/citedrive.R
@@ -4,8 +4,6 @@ require ("stringr")
 # output: R Object
 # Description: Downloads and extracts all DOIs from .bib File
 getDOIsFromBib <- function(citeDriveString){
-  # dummy string
-  citeDriveString <- "https://api.citedrive.com/bib/065aff98-83ac-4d4d-b175-4b4f58ca5bba/references.bib?x=eyJpZCI6ICIwNjVhZmY5OC04M2FjLTRkNGQtYjE3NS00YjRmNThjYTViYmEiLCAidXNlciI6ICI5NzgiLCAic2lnbmF0dXJlIjogImQ1YzBlZWZjYjY2YjBiODBlMGE5YTUzYTg2ZTBhMWFmMzgzM2ViNDU3Yjc5YTdkNjNmOTMxNjc5YjIzNjkxZjgifQ==.bib"
   bibFile <- readLines(citeDriveString)
   DOIs <- bibFile[grepl("doi = ", bibFile)]
   DOIs <- as.data.frame(str_extract(DOIs, "(?<=doi = \\{).*(?=\\})"))
